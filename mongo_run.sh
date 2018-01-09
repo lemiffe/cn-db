6287fd9d56b3d4ebc44445d55fcccb5ff59eeefc
lemiffe <michael.warner@showpad.com>
2018-01-09 17:18:11 +0100

    Test auto-deploy

--- mongo_run.sh
+++ mongo_run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --name cn-db -v ~/mongodata:/data/db -d mongo
+[[ $(docker ps -f 'name=cn-db' --format '{{.Names}}') == 'cn-db' ]] || docker run --name cn-db -v ~/mongodata:/data/db -d mongo
