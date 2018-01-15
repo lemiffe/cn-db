5ca94339e4aa885c5fe64894775a7e4e85cdc197
lemiffe <michael.warner@showpad.com>
2018-01-15 17:23:16 +0100

    Bugfix: Convert test script to python2.7 + fix config.ini copy on deploy

--- test_mongo.py
+++ test_mongo.py
@@ -1,17 +1,25 @@
+# Note: Python 2.7+
+
 from pymongo import MongoClient
-from urllib import parse as urlparse
-import configparser
-config = configparser.ConfigParser()
+from urlparse import urlparse
+import ConfigParser
+
+# Read config
+config = ConfigParser.RawConfigParser()
 config.read('config.ini')
 
-user = urlparse.quote_plus(config['mongo']['db_user'])
-pwd_raw = config['mongo']['db_pwd']
-pwd = urlparse.quote_plus(pwd_raw)
+# Get config variables
+user_raw = config.get('mongo', 'db_user')
+pwd_raw = config.get('mongo', 'db_pwd')
+db_name = config.get('mongo', 'db_name')
+
+# Connect to MongoDB
+user = urlparse(user_raw).path
+pwd = urlparse(pwd_raw).path
 client = MongoClient('localhost', 27017, username=user, password=pwd)
-
-db = client[config['mongo']['db_name']]
-
+db = client[db_name]
 posts = db.test_collection
 
-print('count of entries in ' + config['mongo']['db_name'] + '.test_collection:')
+# Run a quick test (to verify no auth issues)
+print('count of entries in ' + db_name + '.test_collection:')
 print(posts.count())
